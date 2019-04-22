bc14be57077ab12a7d7f5920319fd1ccc4161402
csorgetoomey <csorgetoomey@gmail.com>
2019-04-22 15:33:13 -0700

    Add files via upload

---
+++ map.js
@@ -0,0 +1,15 @@
+var map = L.map('map').setView([47.655548, -122.303200], 15);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiY3NvcmdlIiwiYSI6ImNqb3A2cGMwMzAxbTkzcW9meDIzMDE0ZHMifQ.R5gWO0aBEldQdqU0Nlir-Q',
+}).addTo(map);
+
+L.easyButton( 'fa-crosshairs', function(btn, map){
+
+}).addTo(map);
+
+var geojsonLayer = new L.GeoJSON.AJAX("uw_buildings.json");
+geojsonLayer.addTo(map);
