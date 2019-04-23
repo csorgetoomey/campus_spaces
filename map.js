28401d4551f23e1fc8379124faeabcfe1f1aec7e
csorgetoomey <csorgetoomey@gmail.com>
2019-04-23 13:38:51 -0700

    Add files via upload

--- map.js
+++ map.js
@@ -11,13 +11,8 @@ L.easyButton( 'fa-crosshairs', function(btn, map){
 
 }).addTo(map);
 
-// loading GeoJSON file - Here my html and usa_adm.geojson file resides in same folder
-$.getJSON("uw_buildings.json",function(data){
-// L.geoJson function is used to parse geojson file and load on to map
-L.geoJson(data).addTo(map);
-});
+//needs to be EPSG:4326 when exporting -> save as
 
-$.getJSON("wagda_wagda_UW_SEATTLE_BUILDINGS.json",function(data){
-// L.geoJson function is used to parse geojson file and load on to map
+$.getJSON("uw_buildings.geojson",function(data){
 L.geoJson(data).addTo(map);
 });
