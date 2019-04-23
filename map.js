7e22650482f2476fed27587a5e5d837c6e1f9911
csorgetoomey <csorgetoomey@gmail.com>
2019-04-23 13:46:23 -0700

    Add files via upload

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([47.655548, -122.303200], 15);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+L.tileLayer('', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
