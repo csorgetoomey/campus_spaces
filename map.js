f7f415fce77da194ecba5eb7d0cc9710efa8042d
csorgetoomey <csorgetoomey@gmail.com>
2019-04-23 16:17:19 -0700

    Add files via upload

--- map.js
+++ map.js
@@ -3,14 +3,11 @@
 var map = L.map('map').setView([47.655548, -122.303200], 16);
 
 L.tileLayer('', {
-    //attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    //id: 'mapbox.streets',
-    //accessToken: 'pk.eyJ1IjoiY3NvcmdlIiwiYSI6ImNqb3A2cGMwMzAxbTkzcW9meDIzMDE0ZHMifQ.R5gWO0aBEldQdqU0Nlir-Q',
 }).addTo(map);
 
 L.easyButton( 'fa-crosshairs', function(btn, map){
-    //map.panTo(new L.LatLng(47.655548, -122.303200));
     map.setView(new L.LatLng(47.655548, -122.303200), 16);
 }).addTo(map);
 
